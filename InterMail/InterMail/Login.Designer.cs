4aa01fbec486281ee21da617088ba1aa30668d53
andrestein <andresgrandapiedrhaita@gmail.com>
2016-10-19 15:38:53 -0500

    Añadiendo proyecto

---
+++ InterMail/InterMail/Login.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace InterMail
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el nombre del usuario";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(56, 87);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(405, 20);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(56, 182);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(405, 20);
+            this.txtPass.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ingresa la contraseña";
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Location = new System.Drawing.Point(197, 257);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(117, 23);
+            this.btnInicio.TabIndex = 4;
+            this.btnInicio.Text = "Iniciar sesion";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(371, 307);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(27, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "aqui";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Si no tienes una cuenta creala precionando click";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 353);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Intern E-Mail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ InterMail/InterMail/Login.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OracleClient;
+using System.Data.SqlClient;
+
+
+namespace InterMail
+{
+    public partial class Login : Form
+    {
+        OracleConnection conn = new OracleConnection("Data Source=XE;Persist Security Info=True;User ID=system;Password=andres27041997;Unicode=True");
+        DataSet ds;
+        string SQL;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            try
+            {
+                string user;
+                string pass;                
+                if(txtUsuario.Text != "" && txtPass.Text != "")
+                {
+                    user=txtUsuario.Text;
+                    pass=txtPass.Text;
+                    SQL= "Select * from USUARIO WHERE USERNAME='" + user
+                    + "' and PASS='" + pass + "';";
+                    OracleDataAdapter data = new OracleDataAdapter(SQL, conn);
+                    ds = new DataSet("USUARIO");
+                    data.Fill(ds);                    
+                    if (ds.Tables.Count != 0)
+                    {
+                        MessageBox.Show("Existe el usuario");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Datos invalidos");
+                    }                              
+                }                
+            }
+            catch
+            {
+                MessageBox.Show("No se pudo conectar con la base de datos");
+            }
+        }    
+    }
+}
